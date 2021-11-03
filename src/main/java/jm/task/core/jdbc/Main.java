3cf1c4204d87f7884717e369e03a60a39c4cc1f6
Artem Kirillov <sprigrance@gmail.com>
2021-11-03 12:55:47 +0300

    move connection closing to finally

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args) {
         UserServiceImpl service = new UserServiceImpl();
         service.createUsersTable();
-        service.saveUser("Ivan", "Petrov", (byte) 19);
-        service.saveUser("Vladimir", "Ivanov", (byte) 21);
-        service.saveUser("Elena", "Petrova", (byte) 23);
-        service.saveUser("Michail", "Sidorov", (byte) 27);
+        service.saveUser("Иван", "Петров", (byte) 19);
+        service.saveUser("Петр", "Иванов", (byte) 21);
+        service.saveUser("Елена", "Петрова", (byte) 23);
+        service.saveUser("Михаил", "Петров", (byte) 27);
         service.getAllUsers();
         service.cleanUsersTable();
         service.dropUsersTable();
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -31,50 +31,25 @@ public class UserDaoJDBCImpl implements UserDao {
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-
             try {
                 if (savepoint != null) {
                     connection.rollback(savepoint);
                 } else {
                     connection.rollback();
                 }
-                System.out.println("Executed rollback to savepoint...");
-
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-                System.out.println("PreparedStatement closed successfully");
-
             } catch (SQLException exception) {
-                System.out.println("Наташ, вставай, мы там всё уронили");
+                System.out.println("Не удалось осуществить rollback");
             }
         } finally {
             try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
                 connection.close();
             } catch (SQLException exception) {
-                System.out.println("Наташ, вставай, всё конкретно вообще");
+                System.out.println("Не удалось закрыть соединение");
             }
         }
-//        try (Connection connection = Util.getConnectionWithJDBC();
-//             PreparedStatement preparedStatement = connection.prepareStatement(
-//                     "CREATE TABLE IF NOT EXISTS users (" +
-//                             "id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
-//                             "name VARCHAR(80), " +
-//                             "lastname VARCHAR(100), " +
-//                             "age TINYINT);")) {
-//
-//            Savepoint savepoint1 = connection.setSavepoint();
-//            try {
-//                preparedStatement.execute();
-//                connection.commit();
-//            } catch (SQLException execution) {
-//                System.out.println("SQLException during execution \"createUsersTable()\".\n" +
-//                        "Executing rollback to savepoint1...");
-//                connection.rollback(savepoint1);
-//            }
-//        } catch (SQLException exception) {
-//            System.out.println("SQLException from \"createUsersTable()\" method.");
-//        }
     }
 
     public void dropUsersTable() {
@@ -92,47 +67,25 @@ public class UserDaoJDBCImpl implements UserDao {
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-
             try {
                 if (savepoint != null) {
                     connection.rollback(savepoint);
                 } else {
                     connection.rollback();
                 }
-                System.out.println("Executed rollback to savepoint...");
-
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-                System.out.println("PreparedStatement closed successfully");
-
             } catch (SQLException exception) {
-                System.out.println("Наташ, вставай, мы там всё уронили");
+                System.out.println("Не удалось осуществить rollback");
             }
         } finally {
             try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
                 connection.close();
             } catch (SQLException exception) {
-                System.out.println("Наташ, вставай, всё конкретно вообще");
+                System.out.println("Не удалось закрыть соединение");
             }
         }
-//            try (Connection connection = Util.getConnectionWithJDBC();
-//                 PreparedStatement preparedStatement = connection.prepareStatement(
-//                         "DROP TABLE IF EXISTS users;")) {
-//
-//                Savepoint savepoint2 = connection.setSavepoint();
-//                try {
-//                    preparedStatement.execute();
-//                    connection.commit();
-//                } catch (SQLException execution) {
-//                    System.out.println("SQLException during execution \"dropUsersTable()\".\n" +
-//                            "Executing rollback to savepoint2...");
-//                    connection.rollback(savepoint2);
-//                }
-//
-//            } catch (SQLException exception) {
-//                System.out.println("SQLException during \"dropUsersTable()\" method");
-//            }
     }
 
     public void saveUser(String name, String lastName, byte age) {
@@ -154,51 +107,25 @@ public class UserDaoJDBCImpl implements UserDao {
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-
             try {
                 if (savepoint != null) {
                     connection.rollback(savepoint);
                 } else {
                     connection.rollback();
                 }
-                System.out.println("Executed rollback to savepoint...");
-
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-                System.out.println("PreparedStatement closed successfully");
-
             } catch (SQLException exception) {
-                System.out.println("Наташ, вставай, мы там всё уронили");
+                System.out.println("Не удалось осуществить rollback");
             }
         } finally {
             try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
                 connection.close();
             } catch (SQLException exception) {
-                System.out.println("Наташ, вставай, всё конкретно вообще");
+                System.out.println("Не удалось закрыть соединение");
             }
         }
-//            try (Connection connection = Util.getConnectionWithJDBC();
-//                 PreparedStatement preparedStatement = connection.prepareStatement(
-//                         "INSERT INTO users(name, lastname, age) VALUES (?, ?, ?);")) {
-//
-//                Savepoint savepoint3 = connection.setSavepoint();
-//                try {
-//                    preparedStatement.setString(1, name);
-//                    preparedStatement.setString(2, lastName);
-//                    preparedStatement.setInt(3, age);
-//                    preparedStatement.executeUpdate();
-//                    connection.commit();
-//                    System.out.println("User с именем – " + name + " добавлен в базу данных");
-//                } catch (SQLException execution) {
-//                    System.out.println("SQLException during execution \"saveUser()\".\n" +
-//                            "Executing rollback to savepoint3...");
-//                    connection.rollback(savepoint3);
-//                }
-//
-//            } catch (SQLException exception) {
-//                exception.printStackTrace();
-//            }
     }
 
     public void removeUserById(long id) {
@@ -217,48 +144,25 @@ public class UserDaoJDBCImpl implements UserDao {
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-
             try {
                 if (savepoint != null) {
                     connection.rollback(savepoint);
                 } else {
                     connection.rollback();
                 }
-                System.out.println("Executed rollback to savepoint...");
-
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-                System.out.println("PreparedStatement closed successfully");
-
             } catch (SQLException exception) {
-                System.out.println("Наташ, вставай, мы там всё уронили");
+                System.out.println("Не удалось осуществить rollback");
             }
         } finally {
             try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
                 connection.close();
             } catch (SQLException exception) {
-                System.out.println("Наташ, вставай, всё конкретно вообще");
+                System.out.println("Не удалось закрыть соединение");
             }
         }
-//        try (Connection connection = Util.getConnectionWithJDBC();
-//             PreparedStatement preparedStatement = connection.prepareStatement(
-//                     "DELETE FROM users WHERE id = ?;")) {
-//
-//            Savepoint savepoint4 = connection.setSavepoint();
-//            try {
-//                preparedStatement.setLong(1, id);
-//                preparedStatement.executeUpdate();
-//                connection.commit();
-//            } catch (SQLException execution) {
-//                System.out.println("SQLException during execution \"removeUserById()\".\n" +
-//                        "Executing rollback to savepoint4...");
-//                connection.rollback(savepoint4);
-//            }
-//
-//        } catch (SQLException exception) {
-//            exception.printStackTrace();
-//        }
     }
 
     public List<User> getAllUsers() {
@@ -283,61 +187,35 @@ public class UserDaoJDBCImpl implements UserDao {
 
                 userList.add(new User(id, name, lastName, age));
             }
-            for (User user:userList) {
+            for (User user : userList) {
                 System.out.println(user);
             }
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-
             try {
                 if (savepoint != null) {
                     connection.rollback(savepoint);
                 } else {
                     connection.rollback();
                 }
-                System.out.println("Executed rollback to savepoint...");
-
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-                System.out.println("PreparedStatement closed successfully");
-
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-
             } catch (SQLException exception) {
-                System.out.println("Наташ, вставай, мы там всё уронили");
+                System.out.println("Не удалось осуществить rollback");
             }
         } finally {
             try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
                 connection.close();
             } catch (SQLException exception) {
-                System.out.println("Наташ, вставай, всё конкретно вообще");
+                System.out.println("Не удалось закрыть соединение");
             }
         }
         return userList;
-        //        List<User> userList = new ArrayList<>();
-//
-//        try (Connection connection = Util.getConnectionWithJDBC();
-//             PreparedStatement preparedStatement = connection.prepareStatement(
-//                     "SELECT * FROM users;")) {
-//
-//            ResultSet rs = preparedStatement.executeQuery();
-//            while (rs.next()) {
-//                long id = rs.getLong("id");
-//                String name = rs.getString("name");
-//                String lastName = rs.getString("lastname");
-//                byte age = rs.getByte("age");
-//
-//                userList.add(new User(id, name, lastName, age));
-//            }
-//
-//        } catch (SQLException exception) {
-//            exception.printStackTrace();
-//        }
-//        return userList;
     }
 
 
@@ -349,10 +227,10 @@ public class UserDaoJDBCImpl implements UserDao {
         try {
             savepoint = connection.setSavepoint();
             preparedStatement = connection.prepareStatement(
-                     "TRUNCATE TABLE users;");
+                    "TRUNCATE TABLE users;");
 
-                preparedStatement.execute();
-                connection.commit();
+            preparedStatement.execute();
+            connection.commit();
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
@@ -362,40 +240,19 @@ public class UserDaoJDBCImpl implements UserDao {
                 } else {
                     connection.rollback();
                 }
-                System.out.println("Executed rollback to savepoint...");
-
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-                System.out.println("PreparedStatement closed successfully");
-
             } catch (SQLException exception) {
-                System.out.println("Наташ, вставай, мы там всё уронили");
+                System.out.println("Не удалось осуществить rollback");
             }
         } finally {
             try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
                 connection.close();
             } catch (SQLException exception) {
-                System.out.println("Наташ, вставай, всё конкретно вообще");
+                System.out.println("Не удалось закрыть соединение");
             }
         }
-//        try (Connection connection = Util.getConnectionWithJDBC();
-//             PreparedStatement preparedStatement = connection.prepareStatement(
-//                     "TRUNCATE TABLE users;")) {
-//
-//            Savepoint savepoint5 = connection.setSavepoint();
-//            try {
-//                preparedStatement.execute();
-//                connection.commit();
-//            } catch (SQLException execution) {
-//                System.out.println("SQLException during execution \"cleanUsersTable()\".\n" +
-//                        "Executing rollback to savepoint5...");
-//                connection.rollback(savepoint5);
-//            }
-//
-//        } catch (SQLException exception) {
-//            exception.printStackTrace();
-//        }
     }
 }
 
